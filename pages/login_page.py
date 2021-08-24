572dd675f3ec04fe0ad63445b3f724caa6b0c30f
ScaLseR <scalserstv@yandex.ru>
2021-08-24 17:46:23 +0300

    rename login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -9,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
          # реализуйте проверку на корректный url адрес
-        assert "/login/" in self.url, "Login link is not presented"
+        assert "/login/" in self.browser.current_url, "Login link is not presented"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
