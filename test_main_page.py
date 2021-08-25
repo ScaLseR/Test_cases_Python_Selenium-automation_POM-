2d299e95f4f4f2fe21e39ffac56db124e3e78f9e
ScaLseR <scalserstv@yandex.ru>
2021-08-25 09:13:16 +0300

    add test_enable_login_page and start test

--- test_main_page.py
+++ test_main_page.py
@@ -13,9 +13,8 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_enable_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()
+def test_enable_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
     page.should_be_login_page()
